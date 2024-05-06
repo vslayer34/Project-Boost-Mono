8ccae47d05864d9f7057802793d5c186a9acf5be
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-05-07 00:33:18 +0300

    Added a helper class that contains static refernece to the built-in input maps

---
+++ Scripts/Characters/Player.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+namespace ProjectBoostMono.Scripts.Characters;
+public partial class Player : Node3D
+{
+    private Vector3 _movementVector;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+    public override void _Process(double delta)
+    {
+        
+    }
+}
